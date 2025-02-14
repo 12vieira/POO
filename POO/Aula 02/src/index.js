788ed533cb55dfb2bac35d39cec23c33a9a8a5d7
Vieira <mapivieira@gmail.com>
2025-02-14 15:41:41 -0300

    1402

--- POO/Aula 02/src/index.js
+++ POO/Aula 02/src/index.js
@@ -13,10 +13,49 @@ export function CriarAluno(){
 
     aluno.criar(mat,nome,email,senha)
 }
-
-
-
-
+export function ListarTodos(){
+    const aluno = new AlunoController();
+    aluno.listarTodos();
+}
+export function EditarMatricula(){
+    
+    const aluno = new AlunoController();
+    const matricula = input("Digite a matrícula do aluno: ");
+    aluno.listaPorMatricula(matricula);
+    const resposta = input("Deseja editar o nome? (S/N): ")
+    resposta = resposta.toLocaleLowerCase();
+    if (resposta === "s") {
+        const name = input('Insira o novo nome: ');
+        aluno.nome = name;
+    } else {
+        aluno.nome = aluno.nome;
+    }
+    const resp = input("Deseja editar o e-mail? (S/N): ")
+    resp = resp.toLocaleLowerCase();
+    if (resp === "s") {
+        const email = input('Insira o novo email: ');
+        aluno.email = email;
+    } else {
+        aluno.email = aluno.email;
+    }
+    const rp = input("Deseja editar a senha? (S/N): ")
+    rp = rp.toLocaleLowerCase();
+    if (rp === "s") {
+        const senha = input('Insira a nova senha: ');
+        aluno.senha = senha;
+    } else {
+        aluno.senha = aluno.senha;
+    }
+}
+export function ExcluirTodos(){
+    const aluno = new AlunoController();
+    aluno.deletarTodos();
+}
+export function ExcluirMatricula(){
+    const aluno = new AlunoController();
+    const matricula = input("Digite a matrícula: ")
+    aluno.deletarPorMatricula(matricula);
+}
 export function MenuPrincipal(){
     
     const options = [
@@ -37,19 +76,23 @@ export function MenuPrincipal(){
     switch (choice) {
         case '1':
             console.log('em desenvolvimento...');
-            CriarAluno()
+            CriarAluno();
             break;
         case  '2':
             console.log('em desenvolvimento...');
+            ListarTodos();
             break;
         case '3': 
             console.log('em desenvolvimento...');
+            EditarMatricula();
             break;
         case '4': 
             console.log('em desenvolvimento...');
+            ExcluirTodos();
             break;
         case '5': 
             console.log('em desenvolvimento...');
+            ExcluirMatricula();
             break;
         case '0': 
             break;  
